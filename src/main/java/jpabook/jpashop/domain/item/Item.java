e06f3fb9423391b6bdafc00215680ff6cda2ba6b
lilylemonoh <lilylemonoh@naver.com>
2024-03-18 21:26:52 +0900

    섹션 6 - 주문 도메인 개발 : 주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -33,7 +33,7 @@ public abstract class Item {
         this.stockQuantity += quantity;
     }
 
-    private void removeStock(int quantity) {
+    public void removeStock(int quantity) {
         int restStock = this.stockQuantity - quantity;
         if (restStock < 0) {
             throw new NotEnoughStockException("need more stock");
